5d78135c3fd38223cbedd33324f636f6a9dbb651
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-06-24 12:41:17 +0500

    Update 1.1.cpp

--- pr/1.1.cpp
+++ pr/1.1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 int main() {
@@ -9,6 +10,7 @@ three = 1.732;
 four =2.000;
 five = 2.236;
 cout<<"N"<<'\t'<<"Square Root"<<'\n';
+cout<<fixed<<setprecision(3);
 cout<<"1"<<'\t'<<double(one)<<'\n';
 cout<<"2"<<'\t'<<two<<'\n';
 cout<<"3"<<'\t'<<three<<'\n';
