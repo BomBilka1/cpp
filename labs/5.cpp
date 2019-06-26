7a2fdd0de601f36baca917177150cc87fbcd12eb
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-06-27 00:50:15 +0500

    Update 5.cpp

--- labs/5.cpp
+++ labs/5.cpp
@@ -8,7 +8,22 @@ int main()
             cout << '*';
         cout << '\n';
     }
+
+int n, _min, max, s,count=0, index=0;
  
+cout<<"\nVvedite naturalnoe chislo n=";
+cin>>n;
+_min=n%10;
+for(;n>0;n/=10,count++){
+    s=n%10;
+    if(s<_min){
+        _min=s;
+        index=count;
+    }
+}
+
+cout<<"min: "<<_min<<"\n";
+cout<<"index: "<<count-index;
     
     return 0;
 }
