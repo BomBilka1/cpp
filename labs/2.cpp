d016a12fc430fd471f66cfb93daefacf61420d5a
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-06-28 13:51:52 +0500

    Update 2.cpp

--- labs/2.cpp
+++ labs/2.cpp
@@ -6,8 +6,6 @@ int main() {
 setlocale(0, "");
 double a1, x, b1, a2, b2, y1, y2, l, n, m, p;
 cout<<"y1=a1x+b1\n";
-cout<<"x: ";
-cin>>x;
 cout<<"a1: ";
 cin>>a1;
 cout<<"b1: ";
@@ -17,8 +15,6 @@ cout<<"a2: ";
 cin>>a2;
 cout<<"b2: ";
 cin>>b2;
-y1=a1*x+b1;
-y2=a2*x+b2;
 n=(abs(a2-a1));
 m=(abs(1+a1*a2));
 l=n/m;
@@ -29,22 +25,23 @@ cout<<"Две прямые параллельны";
 else if (a1==-1/a2){
 cout<<"Две прямые перпендикулярны";
 }
+else if (n/m==1){
+cout<<"Угол 45 градусов";
+}
 else{
-cout<<(p*180/M_PI)<<" градусов";
+cout<<"Угол "<<(p*180/M_PI)<<" градусов";
 }
 
 
 //Задание 2
 cout<<"\n----------------------------------------------------------------------";
-int k, a, b, c, d;
-cout<<"\nВведите четырех значное число: "; cin>>k;
+int k, a, b, c, d, j;
+cout<<"\nВведите четырех значное число: "; 
+cin>>k;
 a= k/1000;
 b=(k%1000)/100;
 c=(k%100)/10;
 d=(k%10);
-if((a<b) && (b<c) && (c<d)){
-cout<<"\nTRUE";}
-else{
-cout<<"\nFALSE";}
+(a<b && b<c && c<d && k<10000 && k>999 ) ? cout<<"True" : cout<<"False";
 return 0;
 }
