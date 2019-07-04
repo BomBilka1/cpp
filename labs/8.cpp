6f7eae0c3b92267260bbf38860708f3b1d7fc2ea
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-07-05 03:49:49 +0500

    Update 8.cpp

--- labs/8.cpp
+++ labs/8.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double sumR(double k)
+double prodR(double k1)
 {
 	double slag;
-	if (k>= 1)
+	if (k1>= 1)
 	{
-		slag = ((abs(cos(pow(k,2)-3.8)))/(4.5))-9.7*sin(k-3.1);
-		return slag + sumR(k-1);
+		slag = ((abs(cos(pow(k1,2)-3.8)))/(4.5))-(9.7*sin(k1-3.1));
+		return slag + prodR(k1-1);
 	}
 	else
 		return 1;
 }
-double sum(double k)
+double prod(double k1)
 {
 	double x=1;
-	while(k>=1)
+	while(k1>=1)
 	{
-		x+=((abs(cos(pow(k,2)-3.8)))/(4.5))-9.7*sin(k-3.1);
-		k--;
+		x+=((abs(cos(pow(k1,2)-3.8)))/(4.5))-(9.7*sin(k1-3.1));
+		k1--;
 	}
 	return x;
 }
 int main ()
 {
-	double  n=10;
-	cout << "sumR= " << sumR(n) <<endl;
-	cout << "sum= " << sum(n) <<endl;
+	double  k=10;
+	cout << "prodR= " << prodR(k) <<endl;
+	cout << "prod= " << prod(k) <<endl;
 }
