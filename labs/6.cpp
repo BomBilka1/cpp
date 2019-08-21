2dbcec70fdf4e657accb2f4db64cbfe38d53f049
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-08-21 18:57:50 +0500

    Update 6.cpp

--- labs/6.cpp
+++ labs/6.cpp
@@ -1,28 +1,33 @@
-# include <cmath>
-# include <iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
-int main() {
-    int n=0;
-    double e=0.0001,a=0,an;
-    do {
-        an=a;
-        n++;
-        a=(2*n-1)/pow(2,n);
-    }
-    while (abs(an-a)>=e);
-    cout<<"an = "<<an<<"\n";
-    cout<<"an-1 = "<<a<<"\n";
-    cout<<"n = "<<n<<"\n";
-    
-    n=0;
-    e=0.0001,a=0,an=1;
-    while(abs(an-a)>=e){
-        an=a;
-        n++;
-        a=(1/pow(2,n))+(1/pow(3,n));
-    }
-    cout<<"an = "<<an<<"\n";
-    cout<<"an-1 = "<<a<<"\n";
-    cout<<"n = "<<n<<"\n";
-    return 0;
+int main()
+{
+	//Postuslovie
+	int n=0;
+	double e=0.0001,a=0,an;
+	do
+	{
+		an=a;
+		n++;
+		a=(2*n-1)/pow(2,n);
+	}
+	while (abs(an-a)>=e);
+	cout<<"an = "<<an<<"\n";
+	cout<<"an-1 = " <<a<<"\n";
+	cout<<"n= "<<n<<"\n";
+	//Preduslovie
+	n=0;
+	e=0.0001,a=0,an=1;
+	while(abs(an-a)>=e)
+	{
+		an=a;
+		n++;
+		a=(2*n-1)/pow(2,n);
+	}
+	cout<<"an = "<<an<<"\n";
+	cout<<"an-1 = " <<a<<"\n";
+	cout<<"n= "<<n<<"\n";
+	return 0;
+	
 }
