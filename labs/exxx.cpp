636ba5ae668461ddb09fb8b21ba929436a01cd37
BomBilka! <45636236+BomBilka1@users.noreply.github.com>
2019-09-03 14:11:19 +0500

    Update exxx.cpp

--- labs/exxx.cpp
+++ labs/exxx.cpp
@@ -5,19 +5,25 @@
 using namespace std;
 int summa(int row, int n, int m, double **matrix)
 {
-	double sum = 0, k = 0, f = 0;
-	for (int row = 0; row<m; row++){
+	double sum = 0, k, f = 0, g=0, o=-1;
+	for (int row = 0; row<n; row++){
 	    
-	for (int i = 0; i<n; ++i){
+	for (int i = 0; i<m; ++i){
 		sum += matrix[row][i];
+		cout<<"SUM="<<sum<<" ";
 	}
 	if (f < sum)
 		{
 			f = sum;
-			k+=1;
+			cout<<"F="<<f<<" ";
 		}
+	else{
+		o+=1;
+	}
 		sum=0;
+		g+=1;
 }
+k=g-o;
 return k;
 }
 int main()
@@ -31,7 +37,7 @@ int main()
 	
 	for (int i = 0; i<n; ++i)
 		for (int j = 0; j<m; ++j)
-			a[i][j] = 0+1+rand()%(100-0+1)-(rand()%((int)pow(10,4))/pow(10,4));;
+			a[i][j] = 0+1+rand()%(100-0+1)-(rand()%((int)pow(10,2))/pow(10,2));;
 	for (int i = 0; i<n; ++i)
 	{
 		for (int j = 0; j<m; ++j)
